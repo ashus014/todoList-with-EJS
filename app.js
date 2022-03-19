146d5bd7271b2a81ec8316af9297ba93303262ed
ashuM1 <ashu.ask.singh@gmail.com>
2022-03-19 20:48:38 +0530

    new code changes

--- app.js
+++ app.js
@@ -9,16 +9,14 @@ app.set("view engine", "ejs");
 app.get("/", function(req, res){
 
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
+    var options =  {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
 
-    if(currentDay === 6 || currentDay === 0){
-        day = "Weekend";
-    }
-    else{
-        day = "Weekday";
-    }
-
+    var day = today.toLocaleDateString("en-US", options);
+    
     res.render("list", {kindOfDay: day});
 
 });
