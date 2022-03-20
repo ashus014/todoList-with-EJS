604252808fd77ce8e9980f0dd3c29c9d5ddf00dd
ashuM1 <ashu.ask.singh@gmail.com>
2022-03-20 21:42:50 +0530

    setting up work template

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 
 const express = require("express")
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
+const res = require("express/lib/response");
 
 const app = express();
 let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.set("view engine", "ejs");
 
@@ -28,6 +30,16 @@ app.post("/", function(req, res){
     let item = req.body.newItem;
     items.push(item);
     res.redirect("/");
+});
+
+app.get("/work", function(req, res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.post("/work", function(req, res){
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
 });
 
 app.listen(3000, function(){
