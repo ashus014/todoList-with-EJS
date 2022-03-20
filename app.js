12c2943cfed85b6627db2ac5e25cd3809318bc53
ashuM1 <ashu.ask.singh@gmail.com>
2022-03-20 15:56:48 +0530

    ToDo list working

--- app.js
+++ app.js
@@ -3,28 +3,30 @@ const express = require("express")
 const bodyParser = require("body-parser")
 
 const app = express();
-var item = "";
+let items = ["Buy Food", "Cook Food", "Eat Food"];
 
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
 
-    var today = new Date();
-    var options =  {
+    let today = new Date();
+    let options =  {
         weekday: "long",
         day: "numeric",
         month: "long"
     };
 
-    var day = today.toLocaleDateString("en-US", options);
-    res.render("list", {kindOfDay: day, newListItem: item});
+    let day = today.toLocaleDateString("en-US", options);
+    res.render("list", {listTitle: day, newListItems: items});
 
 });
 
 app.post("/", function(req, res){
-    item = req.body.newItem;
+    let item = req.body.newItem;
+    items.push(item);
     res.redirect("/");
 });
 
