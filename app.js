7a261fd8028a6a9939f69f44876d93542f7eb913
ashuM1 <ashu.ask.singh@gmail.com>
2022-03-31 16:12:28 +0530

    Add git ignore

--- app.js
+++ app.js
@@ -99,7 +99,13 @@ app.post("/work", function(req, res){
     res.redirect("/work");
 });
 
-app.listen(3000, function(){
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function(){
 
     console.log("Server started at Port 3000");
 
