b30b9c5e53979bfac859d38d54d457b6452ae47b
ashuM1 <ashu.ask.singh@gmail.com>
2022-03-31 13:08:43 +0530

    Using Mongoose and deleting with checkbox

--- app.js
+++ app.js
@@ -2,39 +2,91 @@
 const express = require("express")
 const bodyParser = require("body-parser");
 const res = require("express/lib/response");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
-let items = ["Buy Food", "Cook Food", "Eat Food"];
-let workItems = [];
+ //const items = ["Buy Food", "Cook Food", "Eat Food"];
+// const workItems = [];
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+//Connection is established
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+//Schema is created
+const itemsSchema = {
+    name: String
+};
+
+//Module is created
+const Item = mongoose.model("Item", itemsSchema);
+
+//Creating Document
+const item1 = new Item({
+    name: "Welcome..."
+});
+
+const item2 = new Item({
+
+    name: "Hit (+) button to add new item."
+});
+
+const defaultItems = [item1, item2];
+
+
+
 app.get("/", function(req, res){
 
-    let day = date.getDate();
-    res.render("list", {listTitle: day, newListItems: items});
+    //Searching all items
+    Item.find({}, function(err, foundItems){
+
+        if(foundItems.length === 0){
+            Item.insertMany(defaultItems, function(err){
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    console.log("Successfully saved default items to databse.")
+                }
+            });
+            res.redirect("/");
+        }
+        else{
+            res.render("list", {listTitle: "Today", newListItems: foundItems});
+        }
+
+    });
+
+    
 
 });
 
 app.post("/", function(req, res){
 
-    let item = req.body.newItem;
+    const itemName = req.body.newItem;
 
-    if(req.body.button === "Work"){
-        workItems.push(item);
-        res.redirect("/work");
-    }
-    else{
-        items.push(item);
-        res.redirect("/");
-    }
+    const item = new Item({
+        name: itemName
+    });
 
-    console.log(req.body);
+   item.save();
+   res.redirect("/");
     
+});
+
+app.post("/delete", function(req, res){
+
+    const checkedItemId = req.body.checkbox;
+    Item.findByIdAndRemove(checkedItemId, function(err){
+
+        if(!err){
+            console.log("Successfully Deleted...")
+            res.redirect("/");
+        }
+    });
 });
 
 app.get("/work", function(req, res){
